1dc332439e17c5704bbc48f898dfd8f013ac2208
ZHANGSHUYUAN <zhangshuyuan@kingsoft.com>
2021-02-24 11:09:19 +0800

    update: 加载动画更新一下

--- src/components/contentSkeleton/index.tsx
+++ src/components/contentSkeleton/index.tsx
@@ -1,10 +1,8 @@
-import { Card, Row } from 'antd';
-import React from 'react';
-import LoadingSkeleton from '../loadingInput';
+import { Card, Row } from "antd";
+import React from "react";
+import LoadingSkeleton from "../loadingInput";
 
-interface ContentSkeletonProps {
-  
-}
+interface ContentSkeletonProps {}
 
 /** 主Body区域的骨架屏 */
 const ContentSkeleton: React.FC<ContentSkeletonProps> = () => {
@@ -12,24 +10,17 @@ const ContentSkeleton: React.FC<ContentSkeletonProps> = () => {
     <div>
       <Card className="mb-16">
         <Row gutter={16}>
-          <LoadingSkeleton colSpan={8} isLoading/>
-          <LoadingSkeleton colSpan={8} isLoading/>
-          <LoadingSkeleton colSpan={8} isLoading/>
-          <LoadingSkeleton colSpan={8} isLoading/>
-          <LoadingSkeleton colSpan={8} isLoading/>
-        </Row>
-        <Row gutter={16}>
-          <LoadingSkeleton colSpan={2} isLoading/>
-          <LoadingSkeleton colSpan={2} isLoading/>
+          <LoadingSkeleton colSpan={8} isLoading />
+          <LoadingSkeleton colSpan={8} isLoading />
         </Row>
       </Card>
       <Card>
         <Row gutter={16}>
-          <LoadingSkeleton colSpan={24} isLoading/>
-          <LoadingSkeleton colSpan={24} size="small" isLoading/>
-          <LoadingSkeleton colSpan={24} size="small" isLoading/>
-          <LoadingSkeleton colSpan={24} size="small" isLoading/>
-          <LoadingSkeleton colSpan={24} size="small" isLoading/>
+          {Array(100)
+            .fill(0)
+            .map((item, index) => (
+              <LoadingSkeleton key={ index } colSpan={1} isLoading />
+            ))}
         </Row>
       </Card>
     </div>
